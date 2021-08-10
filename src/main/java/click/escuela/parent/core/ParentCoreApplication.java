d39c61036432db799f63db9d17c25275cad77b0b
umbert oscar <oscar.umbert@gmail.com>
2021-08-10 16:46:48 -0300

    create project

---
+++ src/main/java/click/escuela/parent/core/ParentCoreApplication.java
@@ -0,0 +1,13 @@
+package click.escuela.parent.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentCoreApplication.class, args);
+	}
+
+}
