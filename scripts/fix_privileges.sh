0de3618d689a0464cbb390261c785bcc55c4303f
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 20:39:02 -0300

    fix logs

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -5,6 +5,4 @@ chmod +x /home/ec2-user/server/parent-core/server_start.sh
 chmod +x /home/ec2-user/server/parent-core/server_stop.sh
 chmod +x /home/ec2-user/server/parent-core/server_clear.sh
 chmod +x /home/ec2-user/server/parent-core/fix_privileges.sh
-chmod +x /home/ec2-user/server/parent-core/logs
-chmod +x /home/ec2-user/server/parent-core/logs/error.log
-chmod +x /home/ec2-user/server/parent-core/logs/debug.log
+
--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod +x /home/ec2-user/server/parent-core/logs
+chmod +x /home/ec2-user/server/parent-core/logs/error.log
+chmod +x /home/ec2-user/server/parent-core/logs/debug.log
 var="$(cat /home/ec2-user/server/parent-core/parent-service.pid)"
 sudo kill $var
 sudo rm -rf /home/ec2-user/server/parent-core/parent-service.pid
