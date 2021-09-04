925cd48cc95412699f8e9adf7c6648fc752aec4a
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 18:01:19 -0300

    logs

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,4 +5,4 @@ sudo rm -rf /home/ec2-user/server/parent-core/parent-service.pid
 
 echo "eliminando archivo"
 
-sudo java -jar parent-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > parent-service.pid
+sudo java -jar -Dlogging.file.name=/home/ec2-user/server/parent-core/debug.log  parent-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > parent-service.pid
