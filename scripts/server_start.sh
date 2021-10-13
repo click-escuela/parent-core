cc3180db43aacfb8c2abc51bdbdf2d6711486271
Tu Nombre <eve_aragon@hotmail.com>
2021-10-13 14:05:38 -0300

    subir a produccion

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/parent-core
-
-sudo rm -rf /home/ec2-user/server/parent-core/parent-service.pid
-
-echo "eliminando archivo"
-
-sudo java -jar -Dlogging.file.name=/home/ec2-user/server/parent-core/debug.log  parent-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > parent-service.pid
+sudo sh server_start.sh
