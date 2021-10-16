3e82df6ebce843a035fe523b86091052f178f6bd
Evelin <eve_aragon@hotmail.com>
2021-10-16 18:44:06 -0300

    mejora de scripts

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -5,5 +5,6 @@ sudo rm -rf /home/ec2-user/server/parent-core/parent-service.pid
 sudo rm -rf /home/ec2-user/server/parent-core/logs
 sudo rm -rf /home/ec2-user/server/parent-core/server_stop.sh
 sudo rm -rf /home/ec2-user/server/parent-core/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/parent-core/server_prestart.sh
 sudo rm -rf /home/ec2-user/server/parent-core/server_clear.sh
 
